c2aff4f42b1af9b9ba1b455aa005b43f7ed7314b
kenney <kenneyaisho@gmail.com>
2019-11-07 21:45:26 -0500

    added buttons and did some css styles

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Todo } from '../interface/todo';
+import { Component, OnInit } from "@angular/core";
+import { Todo } from "../interface/todo";
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  selector: "app-todo",
+  templateUrl: "./todo.component.html",
+  styleUrls: ["./todo.component.css"]
 })
 export class TodoComponent implements OnInit {
   post: Todo[] = [
-    { task: 'fold clothes', complete: false },
-    { task: 'put clothes in dresser', complete: false },
-    { task: 'relax', complete: false },
-    { task: 'try to pet cat', complete: true },
-    { task: 'pet dog', complete: false },
-    { task: 'be awesome', complete: false }
+    { task: "fold clothes", complete: false },
+    { task: "put clothes in dresser", complete: false },
+    { task: "relax", complete: false },
+    { task: "try to pet cat", complete: true },
+    { task: "pet dog", complete: false },
+    { task: "be awesome", complete: false }
   ];
 
   constructor() {}
